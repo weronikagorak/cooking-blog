cc4e211f10167c48867146ffad4015dd4e5cbd3e
Weronika Górak <weronika.gorak@gmail.com>
2025-08-11 16:30:12 +0200

    pelican update

--- CookingWeb/views.py
+++
@@ -1,9 +0,0 @@
-# CookingWeb/views.py
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to my recipe vault!")
-
-def about(request):
-    return HttpResponse("My name is Weronika Górak and I want to put all the recipes I enjoy cooking in one place.")
